effc934732c28059ef5ac6b1b6328b61b5acab82
Hsinny <hsinnyliu@gmail.com>
2023-04-04 18:28:42 +0800

    feat: 取得所有貼文

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 const http = require("http");
 const mongoose = require("mongoose");
+const headers = require('./helper/headers');
+const Post = require('./models/Post');
 
 dotenv.config({ path: "./.env.local" });
 
@@ -12,9 +14,19 @@ mongoose.connect("mongodb://localhost:27017/posts")
         console.error(error);
     });
 
-const requestListener = (req, res) => {
-    console.log(req.url);
-}
+const requestListener = async (req, res) => {
+
+    // 取得所有貼文
+    if ((req.url === "/posts") && (req.method === 'GET')) {
+        const posts = await Post.find();
+        res.writeHead(200, headers);
+        res.write(JSON.stringify({
+            "status": "success",
+            posts
+        }));
+        res.end();
+    }
+};
 
 const server = http.createServer(requestListener);
 server.listen(process.env.PORT_WEB_SERVER);
