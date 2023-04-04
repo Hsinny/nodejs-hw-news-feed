c188bdb61208a7524592c5f202f71c3ba8e2b53e
Hsinny <hsinnyliu@gmail.com>
2023-04-04 14:19:34 +0800

    feat: 用 mongoose 連線 mongoDB 資料庫

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const http = require("http");
 const dotenv = require("dotenv");
+const http = require("http");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./.env.local" });
+
+mongoose.connect("mongodb://localhost:27017/posts")
+    .then(() => {
+        console.log("資料庫連線成功");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 
 const requestListener = (req, res) => {
     console.log(req.url);
